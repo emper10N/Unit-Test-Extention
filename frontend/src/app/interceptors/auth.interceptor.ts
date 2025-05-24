898be96eec8e24e78d36c4ac7b828b4ba71190e3
emper10N <82743787+emper10N@users.noreply.github.com>
2025-05-24 12:12:42 +0500

    delete useless folders

--- frontend/src/app/interceptors/auth.interceptor.ts
+++
@@ -1,9 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const authInterceptor: HttpInterceptorFn = (request, next) => {
-  const token = localStorage.getItem('token') ?? '';
-  request = request.clone({
-    headers: request.headers.set('Authorization', `Bearer ${token}`),
-  });
-  return next(request);
-};
